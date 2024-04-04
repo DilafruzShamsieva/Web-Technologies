7e00d152b8570877b46ab4abe05c39bab35a6c51
WyattNuge <124921541+WyattNuge@users.noreply.github.com>
2024-04-04 12:22:37 -0500

    Update survey.sql

--- survey.sql
+++ survey.sql
@@ -1,1 +1,20 @@
 --sql file for database
+
+--USE CSCI4410;
+
+--DROP TABLE IF EXISTS Products;
+
+CREATE TABLE Products(
+	Taker_Name VARCHAR(50),
+	Question_One INT(1),
+  Question_Two INT(1),
+  Question_Three INT(1),
+  Question_Four INT(1),
+  Question_Five INT(1),
+  Question_Six INT(1),
+  Question_Seven INT(1),
+  Question_Eight INT(1),
+  Question_Nine INT(1),
+  Question_Ten INT(1),
+	
+);
