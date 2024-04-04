3ba4ddd019cd78231e4f6dc7fdc02bc5231ab372
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-04-04 12:31:52 -0500

    Update survey.php
    Added the Jqurey to the file and also connected the Javascript file

--- survey.php
+++ survey.php
@@ -5,6 +5,8 @@
     <head>
       <title>Mental Maze Survey</title>
         <link type="text/css" rel="stylesheet" href="survey.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+         <script type="text/javascript" src="Survey.js"></script>
     </head>
     
     <body>
