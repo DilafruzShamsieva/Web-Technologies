9f540b7b3a37385af26884a85af11aede0106b0a
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-04-04 12:23:12 -0500

    Update survey.php
    Updated the skeleton of the PHP file so we are good to start. 
    Also connected the Survey.CSS file to the PHP file

--- survey.php
+++ survey.php
@@ -1,1 +1,11 @@
 #main php file
+<!--This  will be the start of the Project-->
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Mental Maze Survey</title>
+        <link type="text/css" rel="stylesheet" href="survey.css" />
+    </head>
+
+    <body>
+</html>
