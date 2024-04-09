7fe0202730ebedc148f730a2f1fd6e9a5272d5e3
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-04-09 12:05:38 -0500

    Updating survey.php
    In the survey.php we are getting general skeleton and the questions down. We are going to have 10 total questions with the answers ranging from 1 (Strongly Disagree) to 5 (Strongly Agree).

--- survey.php
+++ survey.php
@@ -1,6 +1,4 @@
-#main php file
 <!--This  will be the start of the Project-->
-<!-- THis is a new comment-->
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,6 +9,99 @@
     </head>
     
     <body>
+
+      <h2>Survey Form</h2>
+        <form method="post" action="survey_process.php">
+        <p>Question 1: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+      
+        <form method="post" action="survey_process.php">
+        <p>Question 2: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 3: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 4: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 5: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 6: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 7: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 8: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 9: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+
+        <form method="post" action="survey_process.php">
+        <p>Question 10: Do you agree with the statement?</p>
+        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
+        <input type="radio" name="question1" value="Disagree"> Disagree<br>
+        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
+        <input type="radio" name="question1" value="Agree"> Agree<br>
+        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
 
 
         
