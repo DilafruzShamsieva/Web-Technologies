ea434388bc5b5472cdbbfdbd8d826a4e6ef9178d
WyattNuge <yman99230@gmail.com>
2024-04-09 12:10:08 -0500

    Update survey.sql

--- survey.sql
+++ survey.sql
@@ -8,16 +8,16 @@ DROP TABLE IF EXISTS Results;
 
 CREATE TABLE Results(
 	Taker_Name VARCHAR(50),
-	Question_One INT(3),
-  Question_Two INT(3),
-  Question_Three INT(3),
-  Question_Four INT(3),
-  Question_Five INT(3),
-  Question_Six INT(3),
-  Question_Seven INT(3),
-  Question_Eight INT(3),
-  Question_Nine INT(3),
-  Question_Ten INT(3),
+	Question_One INT(8),
+  Question_Two INT(8),
+  Question_Three INT(8),
+  Question_Four INT(8),
+  Question_Five INT(8),
+  Question_Six INT(8),
+  Question_Seven INT(8),
+  Question_Eight INT(8),
+  Question_Nine INT(8),
+  Question_Ten INT(8),
 );
 
 INSERT INTO Results
