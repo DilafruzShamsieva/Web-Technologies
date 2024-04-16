0adb231cbc9d7fa3ae2183d189b0b0bf44352c04
Rogenakhella <116323941+Rogenakhella@users.noreply.github.com>
2024-04-16 12:07:33 -0500

    Update survey.php

--- survey.php
+++ survey.php
@@ -1,22 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <head>
-      <title>Mental Maze Survey</title>
-        <link type="text/css" rel="stylesheet" href="survey.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-         <script type="text/javascript" src="Survey.js"></script>
-    </head>
-    
-    <body>
-      <!--tHIS is THE CORRECT FILE -->
-      <!--We also want to ask about the gender of the person taking the survey-->
-      <!--Welcome page that has the name of the test taker and developer list-->
-      <!-- Ask for a name before displaying the questions-->
+  <head>
+    <title>Mental Maze Survey</title>
+    <link type="text/css" rel="stylesheet" href="survey.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="Survey.js"></script>
+  </head>
 
+  <body>
+    <h2>Mental Maze Survey</h2>
 
-      <h2>Mental Maze Survey</h2>
-
-        <form method="post" action="survey_process.php">
+    <!-- Survey questions - Page 1 -->
+    <form id="survey-form-page1" method="post" action="survey_process.php">
+      <form method="post" action="survey_process.php">
         <p>Question 1: You are not too interested in discussions about various interpretations of creative works.</p>
         <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
         <input type="radio" name="question1" value="Disagree"> Disagree<br>
@@ -61,105 +57,136 @@
         <input type="radio" name="question5" value="Strongly Agree"> Strongly Agree<br>
         <br>
 
-      
-        <p>Question 6: You would love a job that requires you to work alone most of the time.</p>
-        <input type="radio" name="question6" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question6" value="Disagree"> Disagree<br>
-        <input type="radio" name="question6" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question6" value="Agree"> Agree<br>
-        <input type="radio" name="question6" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        
-        <p>Question 7: You prefer tasks that require you to come up with creative solutions rather than follow concrete steps.</p>
-        <input type="radio" name="question7" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question7" value="Disagree"> Disagree<br>
-        <input type="radio" name="question7" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question7" value="Agree"> Agree<br>
-        <input type="radio" name="question7" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        
-        <p>Question 8: You are more likely to rely on emotional intuition than logical reasoning when making a choice.</p>
-        <input type="radio" name="question8" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question8" value="Disagree"> Disagree<br>
-        <input type="radio" name="question8" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question8" value="Agree"> Agree<br>
-        <input type="radio" name="question8" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        
-        <p>Question 9: When faced with a problem, I prefer to trust my instincts and jump into finding a solution.</p>
-        <input type="radio" name="question9" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question9" value="Disagree"> Disagree<br>
-        <input type="radio" name="question9" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question9" value="Agree"> Agree<br>
-        <input type="radio" name="question9" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-      
-        <p>Question 10: When making plans, I tend to be spontaneous and go with the flow.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        <p>Question 11: You find it challenging to express your emotions verbally.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        <p>Question 12: You feel energized after spending time alone, rather than drained.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        <p>Question 13: You often find yourself daydreaming or lost in thought.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        <p>Question 14: You prefer exploring new ideas and concepts rather than sticking to familiar routines.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-
-        <p>Question 15: You believe that intuition plays a significant role in decision-making, alongside logic and reason.</p>
-        <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question10" value="Disagree"> Disagree<br>
-        <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question10" value="Agree"> Agree<br>
-        <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
-        <br>
-      
-        </form>
-
-        <!--We are going to add 5 more questions -->  
-        
-
-
-
-        <input type="submit" value="Submit">
     </form>
 
-    
+    <!-- Survey questions - Page 2 -->
+    <form
+      id="survey-form-page2"
+      method="post"
+      action="survey_process.php"
+      style="display: none"
+    >
+    <p>Question 6: You would love a job that requires you to work alone most of the time.</p>
+    <input type="radio" name="question6" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question6" value="Disagree"> Disagree<br>
+    <input type="radio" name="question6" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question6" value="Agree"> Agree<br>
+    <input type="radio" name="question6" value="Strongly Agree"> Strongly Agree<br>
+    <br>
 
-
-        
-    </body>
     
+    <p>Question 7: You prefer tasks that require you to come up with creative solutions rather than follow concrete steps.</p>
+    <input type="radio" name="question7" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question7" value="Disagree"> Disagree<br>
+    <input type="radio" name="question7" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question7" value="Agree"> Agree<br>
+    <input type="radio" name="question7" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    
+    <p>Question 8: You are more likely to rely on emotional intuition than logical reasoning when making a choice.</p>
+    <input type="radio" name="question8" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question8" value="Disagree"> Disagree<br>
+    <input type="radio" name="question8" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question8" value="Agree"> Agree<br>
+    <input type="radio" name="question8" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    
+    <p>Question 9: When faced with a problem, I prefer to trust my instincts and jump into finding a solution.</p>
+    <input type="radio" name="question9" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question9" value="Disagree"> Disagree<br>
+    <input type="radio" name="question9" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question9" value="Agree"> Agree<br>
+    <input type="radio" name="question9" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+  
+    <p>Question 10: When making plans, I tend to be spontaneous and go with the flow.</p>
+    <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question10" value="Disagree"> Disagree<br>
+    <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question10" value="Agree"> Agree<br>
+    <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+    </form>
+
+    <!-- Survey questions - Page 3 -->
+    <form
+      id="survey-form-page3"
+      method="post"
+      action="survey_process.php"
+      style="display: none"
+    >
+    <p>Question 11: You find it challenging to express your emotions verbally.</p>
+    <input type="radio" name="question11" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question11" value="Disagree"> Disagree<br>
+    <input type="radio" name="question11" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question11" value="Agree"> Agree<br>
+    <input type="radio" name="question11" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    <p>Question 12: You feel energized after spending time alone, rather than drained.</p>
+    <input type="radio" name="question12" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question12" value="Disagree"> Disagree<br>
+    <input type="radio" name="question12" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question12" value="Agree"> Agree<br>
+    <input type="radio" name="question12" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    <p>Question 13: You often find yourself daydreaming or lost in thought.</p>
+    <input type="radio" name="question13" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question13" value="Disagree"> Disagree<br>
+    <input type="radio" name="question13" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question13" value="Agree"> Agree<br>
+    <input type="radio" name="question13" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    <p>Question 14: You prefer exploring new ideas and concepts rather than sticking to familiar routines.</p>
+    <input type="radio" name="question14" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question14" value="Disagree"> Disagree<br>
+    <input type="radio" name="question14" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question14" value="Agree"> Agree<br>
+    <input type="radio" name="question14" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+
+    <p>Question 15: You believe that intuition plays a significant role in decision-making, alongside logic and reason.</p>
+    <input type="radio" name="question15" value="Strongly Disagree"> Strongly Disagree<br>
+    <input type="radio" name="question15" value="Disagree"> Disagree<br>
+    <input type="radio" name="question15" value="Somewhat"> Somewhat<br>
+    <input type="radio" name="question15" value="Agree"> Agree<br>
+    <input type="radio" name="question15" value="Strongly Agree"> Strongly Agree<br>
+    <br>
+    </form>
+
+    <!-- Continue buttons -->
+    <button id="continue-button-page1">Continue</button>
+    <button id="continue-button-page2" style="display: none">Continue</button>
+    <input
+      type="submit"
+      value="Submit"
+      id="submit-button"
+      style="display: none"
+    />
+
+    <!-- jQuery script for handling page transitions -->
+    <script>
+      $(document).ready(function () {
+        $("#continue-button-page1").click(function () {
+          // Hide page 1, show page 2 and the continue button for page 2
+          $("#survey-form-page1").hide();
+          $("#survey-form-page2").show();
+          $("#continue-button-page2").show();
+        });
+
+        $("#continue-button-page2").click(function () {
+          // Hide page 2, show page 3 and the submit button
+          $("#survey-form-page2").hide();
+          $("#survey-form-page3").show();
+          $("#submit-button").show();
+       
+        });
+      });
+    </script>
+  </body>
 </html>
