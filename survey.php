10eb4559b81145083817e2711a65018e8dbd09aa
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-04-18 12:04:02 -0500

    Update Survey.php
    Updating the input tag so that the value contains numbers. We can use the value so that we can sum up the final result.

--- survey.php
+++ survey.php
@@ -18,47 +18,47 @@
     <form id="survey-form-page1" method="post" action="survey_process.php">
       <form method="post" action="survey_process.php">
         <p>Question 1: You are not too interested in discussions about various interpretations of creative works.</p>
-        <input type="radio" name="question1" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question1" value="Disagree"> Disagree<br>
-        <input type="radio" name="question1" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question1" value="Agree"> Agree<br>
-        <input type="radio" name="question1" value="Strongly Agree"> Strongly Agree<br>
+        <input type="radio" name="question1" value=1> Strongly Disagree<br>
+        <input type="radio" name="question1" value=2> Disagree<br>
+        <input type="radio" name="question1" value=3> Somewhat<br>
+        <input type="radio" name="question1" value=4> Agree<br>
+        <input type="radio" name="question1" value=5> Strongly Agree<br>
         <br>
       
         
         <p>Question 2: You enjoy solitary hobbies or activities more than group ones.</p>
-        <input type="radio" name="question2" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question2" value="Disagree"> Disagree<br>
-        <input type="radio" name="question2" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question2" value="Agree"> Agree<br>
-        <input type="radio" name="question2" value="Strongly Agree"> Strongly Agree<br>
+        <input type="radio" name="question2" value=1> Strongly Disagree<br>
+        <input type="radio" name="question2" value=2> Disagree<br>
+        <input type="radio" name="question2" value=3> Somewhat<br>
+        <input type="radio" name="question2" value=4> Agree<br>
+        <input type="radio" name="question2" value=5> Strongly Agree<br>
         <br>
 
         
         <p>Question 3: You can easily connect with people you have just met.</p>
-        <input type="radio" name="question3" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question3" value="Disagree"> Disagree<br>
-        <input type="radio" name="question3" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question3" value="Agree"> Agree<br>
-        <input type="radio" name="question3" value="Strongly Agree"> Strongly Agree<br>
+        <input type="radio" name="question3" value=1> Strongly Disagree<br>
+        <input type="radio" name="question3" value=2> Disagree<br>
+        <input type="radio" name="question3" value=3> Somewhat<br>
+        <input type="radio" name="question3" value=4> Agree<br>
+        <input type="radio" name="question3" value=5> Strongly Agree<br>
         <br>
 
         
         <p>Question 4: Your emotions control you more than you control them.</p>
-        <input type="radio" name="question4" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question4" value="Disagree"> Disagree<br>
-        <input type="radio" name="question4" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question4" value="Agree"> Agree<br>
-        <input type="radio" name="question4" value="Strongly Agree"> Strongly Agree<br>
+        <input type="radio" name="question4" value=1> Strongly Disagree<br>
+        <input type="radio" name="question4" value=2> Disagree<br>
+        <input type="radio" name="question4" value=3> Somewhat<br>
+        <input type="radio" name="question4" value=4> Agree<br>
+        <input type="radio" name="question4" value=5> Strongly Agree<br>
         <br>
 
         
         <p>Question 5: When making decisions, you focus more on how the affected people might feel than on what is most logical or efficient.</p>
-        <input type="radio" name="question5" value="Strongly Disagree"> Strongly Disagree<br>
-        <input type="radio" name="question5" value="Disagree"> Disagree<br>
-        <input type="radio" name="question5" value="Somewhat"> Somewhat<br>
-        <input type="radio" name="question5" value="Agree"> Agree<br>
-        <input type="radio" name="question5" value="Strongly Agree"> Strongly Agree<br>
+        <input type="radio" name="question5" value=1> Strongly Disagree<br>
+        <input type="radio" name="question5" value=2> Disagree<br>
+        <input type="radio" name="question5" value=3> Somewhat<br>
+        <input type="radio" name="question5" value=4> Agree<br>
+        <input type="radio" name="question5" value=5> Strongly Agree<br>
         <br>
 
     </form>
@@ -71,47 +71,47 @@
       style="display: none"
     >
     <p>Question 6: You would love a job that requires you to work alone most of the time.</p>
-    <input type="radio" name="question6" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question6" value="Disagree"> Disagree<br>
-    <input type="radio" name="question6" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question6" value="Agree"> Agree<br>
-    <input type="radio" name="question6" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question8" value=1> Strongly Disagree<br>
+    <input type="radio" name="question8" value=2> Disagree<br>
+    <input type="radio" name="question8" value=3> Somewhat<br>
+    <input type="radio" name="question8" value=4> Agree<br>
+    <input type="radio" name="question8" value=5> Strongly Agree<br>
     <br>
 
     
     <p>Question 7: You prefer tasks that require you to come up with creative solutions rather than follow concrete steps.</p>
-    <input type="radio" name="question7" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question7" value="Disagree"> Disagree<br>
-    <input type="radio" name="question7" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question7" value="Agree"> Agree<br>
-    <input type="radio" name="question7" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question7" value=1> Strongly Disagree<br>
+    <input type="radio" name="question7" value=2> Disagree<br>
+    <input type="radio" name="question7" value=3> Somewhat<br>
+    <input type="radio" name="question7" value=4> Agree<br>
+    <input type="radio" name="question7" value=5> Strongly Agree<br>
     <br>
 
     
     <p>Question 8: You are more likely to rely on emotional intuition than logical reasoning when making a choice.</p>
-    <input type="radio" name="question8" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question8" value="Disagree"> Disagree<br>
-    <input type="radio" name="question8" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question8" value="Agree"> Agree<br>
-    <input type="radio" name="question8" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question8" value=1> Strongly Disagree<br>
+    <input type="radio" name="question8" value=2> Disagree<br>
+    <input type="radio" name="question8" value=3> Somewhat<br>
+    <input type="radio" name="question8" value=4> Agree<br>
+    <input type="radio" name="question8" value=5> Strongly Agree<br>
     <br>
 
     
     <p>Question 9: When faced with a problem, I prefer to trust my instincts and jump into finding a solution.</p>
-    <input type="radio" name="question9" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question9" value="Disagree"> Disagree<br>
-    <input type="radio" name="question9" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question9" value="Agree"> Agree<br>
-    <input type="radio" name="question9" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question9" value=1> Strongly Disagree<br>
+    <input type="radio" name="question9" value=2> Disagree<br>
+    <input type="radio" name="question9" value=3> Somewhat<br>
+    <input type="radio" name="question9" value=4> Agree<br>
+    <input type="radio" name="question9" value=5> Strongly Agree<br>
     <br>
 
   
     <p>Question 10: When making plans, I tend to be spontaneous and go with the flow.</p>
-    <input type="radio" name="question10" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question10" value="Disagree"> Disagree<br>
-    <input type="radio" name="question10" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question10" value="Agree"> Agree<br>
-    <input type="radio" name="question10" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question10" value=1> Strongly Disagree<br>
+    <input type="radio" name="question10" value=2> Disagree<br>
+    <input type="radio" name="question10" value=3> Somewhat<br>
+    <input type="radio" name="question10" value=4> Agree<br>
+    <input type="radio" name="question10" value=5> Strongly Agree<br>
     <br>
     </form>
 
@@ -131,35 +131,35 @@
     <br>
 
     <p>Question 12: You feel energized after spending time alone, rather than drained.</p>
-    <input type="radio" name="question12" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question12" value="Disagree"> Disagree<br>
-    <input type="radio" name="question12" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question12" value="Agree"> Agree<br>
-    <input type="radio" name="question12" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question12" value=1> Strongly Disagree<br>
+    <input type="radio" name="question12" value=2> Disagree<br>
+    <input type="radio" name="question12" value=3> Somewhat<br>
+    <input type="radio" name="question12" value=4> Agree<br>
+    <input type="radio" name="question12" value=5> Strongly Agree<br>
     <br>
 
     <p>Question 13: You often find yourself daydreaming or lost in thought.</p>
-    <input type="radio" name="question13" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question13" value="Disagree"> Disagree<br>
-    <input type="radio" name="question13" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question13" value="Agree"> Agree<br>
-    <input type="radio" name="question13" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question13" value=1> Strongly Disagree<br>
+    <input type="radio" name="question13" value=2> Disagree<br>
+    <input type="radio" name="question13" value=3> Somewhat<br>
+    <input type="radio" name="question13" value=4> Agree<br>
+    <input type="radio" name="question13" value=5> Strongly Agree<br>
     <br>
 
     <p>Question 14: You prefer exploring new ideas and concepts rather than sticking to familiar routines.</p>
-    <input type="radio" name="question14" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question14" value="Disagree"> Disagree<br>
-    <input type="radio" name="question14" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question14" value="Agree"> Agree<br>
-    <input type="radio" name="question14" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question14" value=1> Strongly Disagree<br>
+    <input type="radio" name="question14" value=2> Disagree<br>
+    <input type="radio" name="question14" value=3> Somewhat<br>
+    <input type="radio" name="question14" value=4> Agree<br>
+    <input type="radio" name="question14" value=5> Strongly Agree<br>
     <br>
 
     <p>Question 15: You believe that intuition plays a significant role in decision-making, alongside logic and reason.</p>
-    <input type="radio" name="question15" value="Strongly Disagree"> Strongly Disagree<br>
-    <input type="radio" name="question15" value="Disagree"> Disagree<br>
-    <input type="radio" name="question15" value="Somewhat"> Somewhat<br>
-    <input type="radio" name="question15" value="Agree"> Agree<br>
-    <input type="radio" name="question15" value="Strongly Agree"> Strongly Agree<br>
+    <input type="radio" name="question15" value=1> Strongly Disagree<br>
+    <input type="radio" name="question15" value=2> Disagree<br>
+    <input type="radio" name="question15" value=3> Somewhat<br>
+    <input type="radio" name="question15" value=4> Agree<br>
+    <input type="radio" name="question15" value=5> Strongly Agree<br>
     <br>
     </form>
 
